ca83c13672c00ceb2c44e3c05a55f72c4fa35d32
HORUS <HORUS@DESKTOP-Q1LD7DJ>
2025-11-15 07:03:42 +0200

    Add project files.

---
+++ CSharpAdvanced/Program.cs
@@ -0,0 +1,23 @@
+﻿#region Delegates
+
+namespace CSharpAdvanced
+{
+    internal class Program
+    {
+        delegate int MathOperation(int a, int b);
+        static void PerformOperation(int a, int b, MathOperation operation)
+        {
+            int result = operation(a, b);
+            Console.WriteLine($"Result: {result}");
+        }
+        static void Main(string[] args)
+        {
+            PerformOperation(10, 5, (x, y) => x + y); // Addition // Lambda Expression
+            PerformOperation(10, 5, (x, y) => x - y); // Subtraction
+            PerformOperation(10, 5, (x, y) => x * y); // Multiplication
+            PerformOperation(10, 5, (x, y) => x / y); // Division
+        }
+    }
+}
+
+#endregion
